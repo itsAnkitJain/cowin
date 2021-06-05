ff66559c52904fbc7bd8456d509aeed5909634c3
Administrator <jnankit3@gmail.com>
2021-06-05 18:19:38 +0530

    added test

--- cowin.py
+++ cowin.py
@@ -5,15 +5,18 @@ import os
 from email.message import EmailMessage
 from datetime import date
 
+# Gets date in required format
 today_date = date.today()
 year = today_date.year
 month = today_date.month
 dayto = today_date.day
 date = f"{dayto}-{month}-{year}"
 
+# Setup email address and password as environment variables
 EMAIL_ADDR = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
+# Change with your desired district id
 district_id = 143
 
 url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={date}"
@@ -21,10 +24,14 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 response = requests.get(url, headers=headers)
 availability = response.json()
 
+# SMTP connection for email
 smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 smtpObj.login(EMAIL_ADDR, EMAIL_PASSWORD)
 
 def send_email():
+    """
+    Sends email
+    """
     msg = EmailMessage()
     msg['From'] = EMAIL_ADDR
     msg['To'] = '<email-id>'
@@ -32,6 +39,7 @@ def send_email():
     msg.set_content(content)
     smtpObj.send_message(msg)
 
+# Gets required data and sends email
 for i in range(len(availability["centers"])):
     for j in range(len(availability["centers"][i]["sessions"])):
         min_age_limit = availability["centers"][i]["sessions"][j]["min_age_limit"]
